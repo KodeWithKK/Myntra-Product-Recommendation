413c6bd157bf5ffacac8051bb88b038f122bc271
KodeWithKK <kodewithkk@gmail.com>
2025-06-21 09:18:52 +0530

    feat: increase memory size for AWS Lambda function to 2048 MB

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "backend_fn" {
   image_uri     = "${aws_ecr_repository.backend.repository_url}:latest"
   role          = aws_iam_role.lambda_exec.arn
   timeout       = 30
-  memory_size   = 1024 # RAM size in MB
+  memory_size   = 2048 # RAM size in MB
 }
 
 resource "aws_lambda_function_url" "backend_url" {
