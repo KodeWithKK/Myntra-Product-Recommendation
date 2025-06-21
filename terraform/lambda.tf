2190bf128abd6a4632ac6c9656dcba2749beecc2
KodeWithKK <kodewithkk@gmail.com>
2025-06-21 08:50:47 +0530

    feat: enhance AWS Lambda deployment workflow with Terraform image readiness check

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,22 @@
+resource "null_resource" "wait_for_image" {
+  triggers = {
+    image_pushed = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecr describe-images \
+        --repository-name myntra-prs-backend \
+        --image-ids imageTag=latest \
+        --region ap-south-1 \
+        || (echo "Image not yet available" && exit 1)
+    EOT
+  }
+}
+
+
 resource "aws_lambda_function" "backend_fn" {
+  depends_on    = [null_resource.wait_for_image]
   function_name = "myntra-prs-backend"
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.backend.repository_url}:latest"
